f6f48ceb7fd8cf750bcc2fb6df5aa42349384de7
Lechonita <jessicarouillon@hotmail.com>
2023-11-22 19:21:56 +0100

    start

--- Module00/ex00/megaphone.cpp
+++ Module00/ex00/megaphone.cpp
@@ -1,17 +1,21 @@
 #include <iostream>
 
-int	main(int ac, char **av)
+int main(int ac, char **av)
 {
-	std::string const	no_arg = "* LOUD AND UNBEARABLE FEEDBACK NOISE *";
-	std::string			arg;
+	std::string const no_arg = "* LOUD AND UNBEARABLE FEEDBACK NOISE *";
+	std::string arg;
 
 	if (ac == 1)
 	{
 		std::cout << no_arg << std::endl;
 		return;
 	}
-	else if (ac > 1)
+	for (int i = 1; i < ac; i++)
 	{
-		
+		arg = av[i];
+		for (int j = 0; j < arg.length(); j++)
+		{
+			std::cout << (char)toupper(arg[j]);
+		}
 	}
 }
