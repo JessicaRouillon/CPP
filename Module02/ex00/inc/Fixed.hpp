3ef8d996e39a6b1c765e9287ba1431252ef72b7d
Lechonita <jessicarouillon@hotmail.com>
2023-12-06 17:17:12 +0100

    ex00 finished

---
+++ Module02/ex00/inc/Fixed.hpp
@@ -0,0 +1,24 @@
+#ifndef FIXED_HPP
+# define FIXED_HPP
+
+#include <iostream>
+
+class Fixed
+{
+public:
+
+	Fixed(); // constructor
+	Fixed(const Fixed& copy); // copy constructor
+	Fixed& operator=(const Fixed& src); // move assignment operator
+	~Fixed(); // destructor
+
+	int getRawBits( void ) const;
+	void setRawBits( int const raw );
+
+private:
+
+	int					_fixedPointNbValue;
+	static const int	_fractBitsNb;
+};
+
+#endif
---
+++ Module02/ex00/src/Fixed.cpp
@@ -0,0 +1,53 @@
+#include "Fixed.hpp"
+
+/********************************************************************************/
+/*************************** CONSTRUCTOR / DESTRUCTOR ***************************/
+/********************************************************************************/
+
+const int Fixed::_fractBitsNb = 8;
+
+// Constructor
+Fixed::Fixed()
+{
+	std::cout << "Default constructor called" << std::endl;
+	this->_fixedPointNbValue = 0;
+}
+
+// Copy constructor
+Fixed::Fixed(const Fixed& copy)
+{
+	std::cout << "Copy constructor called" << std::endl;
+	*this = copy;
+}
+
+// Destructor
+Fixed::~Fixed()
+{
+	std::cout << "Destructor called" << std::endl;
+}
+
+// Move assignment operator
+Fixed &Fixed::operator=(const Fixed& src)
+{
+	std::cout << "Copy assignment operator called" << std::endl;
+	if (this != &src)
+		this->_fixedPointNbValue = src.getRawBits();
+	return (*this);
+}
+
+
+/********************************************************************************/
+/***************************** MEMBER FUNCTIONS *********************************/
+/********************************************************************************/
+
+int	Fixed::getRawBits( void ) const
+{
+	std::cout << "getRawBits member function called" << std::endl;
+	return (this->_fixedPointNbValue);
+}
+
+void	Fixed::setRawBits( int const raw )
+{
+	this->_fixedPointNbValue = raw;
+	// std::cout << raw << std::endl;
+}
