4e01af7f45aac0624fd932bae3ea7acea972e9c5
Lechonita <jessicarouillon@hotmail.com>
2024-01-09 12:12:48 +0100

    adding module05

---
+++ Module05/ex00/inc/Bureaucrat.hpp
@@ -0,0 +1,20 @@
+#ifndef BUREAUCRAT_HPP
+#define BUREAUCRAT_HPP
+
+#include <iostream>
+
+class Bureaucrat
+{
+
+public:
+	Bureaucrat();
+	~Bureaucrat();
+
+	std::string		getName();
+	
+
+private:
+
+};
+
+#endif
