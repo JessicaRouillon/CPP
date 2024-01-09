60eb9c64eb4a52b0678cc1f674d81cf3b3e48e73
Lechonita <jessicarouillon@hotmail.com>
2024-01-09 17:59:04 +0100

    ex00 finished

--- Module05/ex00/inc/Bureaucrat.hpp
+++ Module05/ex00/inc/Bureaucrat.hpp
@@ -7,14 +7,53 @@ class Bureaucrat
 {
 
 public:
+// Constructors & Destructor
 	Bureaucrat();
+	Bureaucrat(const std::string name, const int grade);
+	Bureaucrat(const Bureaucrat& copy);
 	~Bureaucrat();
 
-	std::string		getName();
-	
+	Bureaucrat& operator=(const Bureaucrat& src);
+
+// Getters
+	std::string		getName() const;
+	int				getGrade() const;
+
+// Increment & Decrement
+	void			incrementGrade();
+	void			decrementGrade();
+
+// Exceptions
+	class Exception : public std::exception
+	{
+		public :
+			const char*		what() const throw();
+	};
+
+	class GradeTooHighException : public Bureaucrat::Exception
+	{
+		public :
+			const char*		what() const throw();
+	};
+
+	class GradeTooLowException : public Bureaucrat::Exception
+	{
+		public :
+			const char*		what() const throw();
+	};
+
+	class InvalidName : public Bureaucrat::Exception
+	{
+		public :
+			const char*		what() const throw();
+	};
 
 private:
 
+	std::string		_name;
+	int				_grade;
 };
+
+std::ostream&	operator<<(std::ostream& output_stream, Bureaucrat const &bureaucrat);
 
 #endif
