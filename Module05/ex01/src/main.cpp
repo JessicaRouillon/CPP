1839040bf9bb346ba39d3d7c5d3edead7ca0466a
Lechonita <jessicarouillon@hotmail.com>
2024-01-10 14:06:37 +0100

    ex01 deleted previous tests

--- Module05/ex01/src/main.cpp
+++ Module05/ex01/src/main.cpp
@@ -2,142 +2,7 @@
 #include "Form.hpp"
 
 int	main()
-{	
-	std::cout << "\033[34m\t================ BUREAUCRAT TESTS ================\033[0m" << std::endl << std::endl;
-
-	std::cout << "\033[34m1/Construction with grade too low\033[0m" << std::endl;
-	try
-	{
-		Bureaucrat	bob("Bob", -75);
-	}
-	catch (Bureaucrat::Exception& e)
-	{
-		std::cout << e.what();
-	}
-	std::cout << std::endl;
-	std::cout << std::endl;
-
-// -----------------------------------------------------------------------------
-
-	std::cout << "\033[34m2/Construction with grade too high\033[0m" << std::endl;
-	try
-	{
-		Bureaucrat	bob("Bob", 151);
-	}
-	catch (Bureaucrat::Exception& e)
-	{
-		std::cout << e.what();
-	}
-	std::cout << std::endl;
-	std::cout << std::endl;
-
-// -----------------------------------------------------------------------------
-
-	std::cout << "\033[34m3/Construction with no name\033[0m" << std::endl;
-	try
-	{
-		Bureaucrat	bob("", 42);
-	}
-	catch (Bureaucrat::Exception& e)
-	{
-		std::cout << e.what();
-	}
-	std::cout << std::endl;
-	std::cout << std::endl;
-
-// -----------------------------------------------------------------------------
-
-	std::cout << "\033[34m4/Default Construction\033[0m" << std::endl;
-	try
-	{
-		Bureaucrat	ref;
-		std::cout << ref << std::endl;
-	}
-	catch (Bureaucrat::Exception& e)
-	{
-		std::cout << e.what();
-	}
-	std::cout << std::endl;
-	std::cout << std::endl;
-
-// -----------------------------------------------------------------------------
-
-	std::cout << "\033[34m5/Correct Construction\033[0m" << std::endl;
-	try
-	{
-		Bureaucrat	bob("Bob", 75);
-		std::cout << bob << std::endl;
-	}
-	catch (Bureaucrat::Exception& e)
-	{
-		std::cout << e.what();
-	}
-	std::cout << std::endl;
-	std::cout << std::endl;
-
-// -----------------------------------------------------------------------------
-
-	std::cout << "\033[34m6/Correct Incrementation\033[0m" << std::endl;
-	Bureaucrat	jack("Jack", 10);
-	try
-	{
-		jack.incrementGrade();
-	}
-	catch (Bureaucrat::Exception& e)
-	{
-		std::cout << e.what();
-	}
-	std::cout << std::endl;
-	std::cout << std::endl;
-
-// -----------------------------------------------------------------------------
-
-	std::cout << "\033[34m7/Correct Decrementation\033[0m" << std::endl;
-	try
-	{
-		jack.decrementGrade();
-	}
-	catch (Bureaucrat::Exception& e)
-	{
-		std::cout << e.what();
-	}
-	std::cout << std::endl;
-	std::cout << std::endl;
-
-// -----------------------------------------------------------------------------
-
-	std::cout << "\033[34m8/Failed Incrementation\033[0m" << std::endl;
-	Bureaucrat	jacky("Jacky", 1);
-	try
-	{
-		jacky.incrementGrade();
-	}
-	catch (Bureaucrat::Exception& e)
-	{
-		std::cout << e.what();
-	}
-	std::cout << std::endl;
-	std::cout << std::endl;
-
-// -----------------------------------------------------------------------------
-
-	std::cout << "\033[34m9/Failed Decrementation\033[0m" << std::endl;
-	Bureaucrat	franky("Franky", 150);
-	try
-	{
-		franky.decrementGrade();
-	}
-	catch (Bureaucrat::Exception& e)
-	{
-		std::cout << e.what();
-	}
-	std::cout << std::endl;
-	std::cout << std::endl;
-
-// =============================================================================
-
-	std::cout << "\033[34m\t================ FORM TESTS ================\033[0m" << std::endl << std::endl;
-
+{
 	std::cout << "\033[34m1/Construction with grade too low (sign)\033[0m" << std::endl;
 	try
 	{
@@ -258,6 +123,7 @@ int	main()
 // -----------------------------------------------------------------------------
 
 	std::cout << "\033[34m8/Double Signature\033[0m" << std::endl;
+	Bureaucrat	jack("Jack", 10);
 	try
 	{
 		nda.beSigned(jack);
